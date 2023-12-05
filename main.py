0dde108a6859882ec6ed3d0182e920ef0f12642d
yehordanilenko <egordanilenkooo@gmail.com>
2023-12-05 22:50:55 +0100

    Adding List of SKUs with some D but 0 FCST

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from collections import defaultdict
-
 import pandas as pd
 from datetime import datetime
 import math
 from statistics import mean
 
 file_delfor = 'DELFOR2023.10.15.csv' # Name of delfor file
+file_demand = 'demand_file.xlsx'  # Name of demand file
+
 
 data_delfor = pd.read_csv(file_delfor, sep='|', header=None, skiprows=2) # Reading data from csv
 data_delfor.columns = ['Col1','Col2','Col3','Col4','Col5','PrimeItem','Col7','Col8','Col9','Col10','Qty', 'Date', 'Col13'] # Assign a name for columns
@@ -23,7 +24,9 @@ data_delfor['Qty'] = data_delfor['Qty'].fillna(-1).astype(int)
 data_delfor['Date'] = data_delfor['Date'].fillna(-1).astype(int)
 
 all_FC_list = data_delfor.values.tolist() # Convert our delfor dataframe to list
+print("====================================================================================")
 print("Length of FC delfor: ", len(all_FC_list))
+print("====================================================================================")
 
 for r in all_FC_list:
     date_number = r[2]  # Replace this with your integer representing the date
@@ -34,21 +37,18 @@ for r in all_FC_list:
     r[2] = dd                                   # convert int to data
 
 
-file_demand = 'demand_file.xlsx'  # Name of demand file
-
 # Read the Excel file into a Pandas DataFrame
 data_demand = pd.read_excel(file_demand, sheet_name='Closed_SSD')
 data_demand_open = pd.read_excel(file_demand, sheet_name='Open_SSD')
-print('**************************************')
-print(data_demand_open)
-print('**************************************')
 l1 = data_demand.values.tolist()  # list of demand file I work with him for finding number of weeks
 l2 = data_demand_open.values.tolist()
 
 sorted_list = sorted(l1, key=lambda x: x[1])  # sorted by data for getting n
 n = (sorted_list[len(sorted_list)-1])[1].date() - (sorted_list[0])[1].date() # At first n is number of days between first and last dates
 n = (n/7).days + 1 # We change n to count number of weeks
+print("====================================================================================")
 print("Number of weeks: ", n)
+print("====================================================================================")
 
 list_of_demand_all = []
 
@@ -56,8 +56,8 @@ for i in range(len(l1)):
     for j in range(len(l2)):
         if(l1[i][0] == l2[j][0] and l1[i][1] == l2[j][1]):
             l1[i][2] += l2[j][2]
-print(len(l1))
-l11 = l1
+
+
 for i in range(len(l2)):
     temp_c = 0
     for j in range(len(l1)):
@@ -65,16 +65,21 @@ for i in range(len(l2)):
             temp_c += 1
     if(temp_c == 0):
         l1.append(l2[i])
-print(len(l1))
+
+print("====================================================================================")
+print("Number of demand data: ",len(l1))
+print("====================================================================================")
 
 all_rows_as_list = l1 # data_demand.values.tolist() # dataframe demand to list
 
 sorted_list_delfor = sorted(all_FC_list, key=lambda x: x[2]) # Sorting delfor by date
 sorted_list_demand = sorted(all_rows_as_list, key=lambda x: x[1]) # Sorting demand by date
-print("First element of delfor and demand list: ")
-print(sorted_list_delfor[0], sorted_list_demand[0])
+# print("First element of delfor and demand list: ")
+# print(sorted_list_delfor[0], sorted_list_demand[0])
+print("====================================================================================")
 print("Size of delfor list and size of demand list: ")
 print(len(all_FC_list), len(all_rows_as_list))
+print("====================================================================================")
 
 
 spec_data = (sorted_list_demand[len(sorted_list_demand) - 1])[1] # last date in demand list
@@ -85,8 +90,10 @@ for i in range(len(all_FC_list)):
     if((all_FC_list[i])[2] <= spec_data ):
         arr1.append(all_FC_list[i])
 
+print("====================================================================================")
 print("Amount items in delfor with dates before last date in demand: ")
 print(len(arr1))
+print("====================================================================================")
 
 for l in arr1:
     l[1], l[2] = l[2], l[1]
@@ -104,27 +111,35 @@ arr2 = [item[1] for item in all_rows_as_list]
 arrTe = [item[0] for item in all_rows_as_list]
 finalArray = []
 
-sumDemand = 1
+sumDemand = 0
 
 countABSFCD = 0
 sumDemAllPeriod = 1
+forecast_allPer = 0
 
-
-
+List_of_SKUs_with_some_D_but_0_FCST = []
+list_of_demands_and_delfors = []
 # Основной цикл уже для всего
 for i in range(len(arr1)):
     if ((arr1[i])[0] not in setf):
+        # if(sumDemand > 0 and forecast_allPer == 0):
+        #     List_of_SKUs_with_some_D_but_0_FCST.append((arr1[i])[0])
+
         RMSE = 123 if sumDemand == 0 else math.sqrt(((count*count)/n))
         RMSE_percent = 0 if sumDemAllPeriod == 0 else RMSE/(sumDemAllPeriod/n)
         BIAS = count / n
         MAE = countABSFCD/n
         SCORE = MAE + abs(BIAS)
         SCORE_percent = abs(0 if sumDemand == 0 else count / sumDemand) + (0 if sumDemAllPeriod == 0 else countABSFCD / sumDemAllPeriod)
+        list_of_demands_and_delfors.append([sumDemand, forecast_allPer])
+        last_for_demand = sumDemand
+        last_for_FC = forecast_allPer
         finalArray.append([(arr1[i])[0], BIAS, 0 if sumDemand == 0 else count / sumDemand, MAE , 0 if sumDemAllPeriod == 0 else countABSFCD / sumDemAllPeriod, RMSE, RMSE_percent, SCORE, SCORE_percent])
         count = 0
         sumDemand = 0
         countABSFCD = 0
         sumDemAllPeriod = 0
+        forecast_allPer = 0
 
     setf.add((arr1[i])[0])
 
@@ -134,12 +149,11 @@ for i in range(len(arr1)):
             sumDemand+=(arr1[i])[3]
             countABSFCD += abs((arr1[i])[2] - (arr1[i])[3])
             sumDemAllPeriod+=(arr1[i])[3]
+            forecast_allPer += (arr1[i])[2]
         else:
             count+= (arr1[i])[2]
             countABSFCD += abs((arr1[i])[2])
-
-
-print(sumDemand)
+            forecast_allPer += (arr1[i])[2]
 
 
 for i in range(len(finalArray)-1):
@@ -151,6 +165,8 @@ for i in range(len(finalArray)-1):
     (finalArray[i])[6] = (finalArray[i + 1])[6]
     (finalArray[i])[7] = (finalArray[i + 1])[7]
     (finalArray[i])[8] = (finalArray[i + 1])[8]
+    (list_of_demands_and_delfors[i])[0] = (list_of_demands_and_delfors[i+1])[0]
+    (list_of_demands_and_delfors[i])[1] = (list_of_demands_and_delfors[i+1])[1]
 
 (finalArray[len(finalArray)-1])[1] = count/n
 (finalArray[len(finalArray)-1])[3] = countABSFCD/n
@@ -158,6 +174,15 @@ for i in range(len(finalArray)-1):
 (finalArray[len(finalArray)-1])[6] = RMSE_percent
 (finalArray[len(finalArray)-1])[7] = SCORE
 (finalArray[len(finalArray)-1])[8] = SCORE_percent
+(list_of_demands_and_delfors[i])[0] = last_for_demand
+(list_of_demands_and_delfors[i])[1] = last_for_FC
+
+for i in range(len(list_of_demands_and_delfors)):
+    if(list_of_demands_and_delfors[i][0] > 0 and list_of_demands_and_delfors[i][1] == 0):
+        List_of_SKUs_with_some_D_but_0_FCST.append(finalArray[i][0])
+
+
+
 temp2 = []  # THIS FINAL LIST
 
 for i in range(len(finalArray)):
@@ -168,10 +193,15 @@ for i in range(len(finalArray)):
         (temp2[len(temp2) - 1])[6] = round((finalArray[i])[6] * 100, 1)
         (temp2[len(temp2) - 1])[8] = round((finalArray[i])[8] * 100, 1)
 
-for el in temp2:
-    print(el)
-print(len(temp2))
 
+print("====================================================================================")
+for el in temp2:
+    print(el)             # ЭТО ВСЁ ИТОГОВЫЙ ТО ЧТО ПЕРВАЯ ЗАДАЧА
+print(len(temp2))
+print("====================================================================================")
+
+
+print("====================================================================================")
 print("Так теперь метрики ебать: ")
 #print("Metrics of BIAS: ", mean([item[1] for item in temp2]))
 print("Metrics of BIAS%: ", round(mean([item[2] for item in temp2]), 1))
@@ -181,6 +211,8 @@ print("Metrics of MAE%: ", round(mean([item[4] for item in temp2]), 1))
 print("Metrics of RMSE%: ", round(mean([item[6] for item in temp2]), 1))
 #print("Metrics of SCORE: ", mean([item[7] for item in temp2]))
 print("Metrics of SCORE%: ", round(mean([item[8] for item in temp2]), 1))
+print("====================================================================================")
+
 
 List_of_SKUs_with_demand_downside = []
 List_of_SKUs_with_demand_upside = []
@@ -191,18 +223,26 @@ for el in temp2:
     elif(el[2] < -5):
         List_of_SKUs_with_demand_upside.append([el[0], el[2]])
 
+print("====================================================================================")
 print(f"Count of downside: {len(List_of_SKUs_with_demand_downside)} \nCount of upside: {len(List_of_SKUs_with_demand_upside)}")
 print(f"Count of good bias: {len(temp2) - len(List_of_SKUs_with_demand_downside) - len(List_of_SKUs_with_demand_upside)}")
-
+print("====================================================================================")
+print("====================================================================================")
+print("List demand > 0 and FC = 0: ")
+#print(len(List_of_SKUs_with_some_D_but_0_FCST))
+print(List_of_SKUs_with_some_D_but_0_FCST)
+for el in List_of_SKUs_with_some_D_but_0_FCST:
+    print(el)
+print("====================================================================================")
 #print(List_of_SKUs_with_demand_downside)
 #print(List_of_SKUs_with_demand_upside)
 
 
 list_dates = []
 c2 = 0
-for i in sorted_list_demand:
-    print(i)
-print(len(sorted_list_demand))
+# for i in sorted_list_demand:
+#     print(i)
+#print(len(sorted_list_demand))
 
 sum_by_date = defaultdict(int)
 
@@ -225,86 +265,86 @@ print(totals_only)
 #print(len(all_rows_as_list))
 
 
-integers_list = [sublist[2] for sublist in all_rows_as_list]
-
-from scipy.stats import kendalltau
-import numpy as np
-import matplotlib.pyplot as plt
-from pandas import Timestamp
-
-# Ваш лист с данными
-data = all_rows_as_list
-
-# Преобразование дат в формат Timestamp и количеств в массивы numpy
-dates = np.array([row[1] for row in data])
-qty = np.array([row[2] for row in data])
-
-# Выполнение теста Кендалла
-tau, p_value = kendalltau(qty, range(len(qty)))
-
-# Определение направления тренда
-trend_direction = "Upward" if tau > 0 else "Downward" if tau < 0 else "No Trend"
-
-# Вывод результатов
-print(f"Trend Direction: {trend_direction}")
-print(f"Kendall's Tau: {tau}")
-print(f"P-value: {p_value}")
-
-# Постройте график данных
-plt.plot(dates, qty, marker='o', label='Actual Demand')
-plt.title('Demand Over Time')
-plt.xlabel('Date')
-plt.ylabel('Demand')
-plt.legend()
-plt.show()
-import pandas as pd
-from scipy.stats import kendalltau
-
-# Предположим, что у вас есть DataFrame с данными в листе1
-# Замените 'your_data.csv' на путь к вашему файлу или используйте другие способы загрузки данных
-
-# Предположим, что у вас есть два столбца 'X' и 'Y', и вы хотите проверить тренд в 'Y' относительно 'X'
-x_values = totals_only
-y_values = [num for num in range(len(totals_only))]
-
-# Выполняем тест Кендалла
-tau, p_value = kendalltau(x_values, y_values)
-
-# Выводим результаты теста
-print(f"Значение статистики Кендалла (τ): {tau}")
-print(f"P-значение: {p_value}")
-
-# Проверяем значимость
-if p_value < 0.05:
-    print("Отвергаем нулевую гипотезу, есть тренд.")
-else:
-    print("Нет оснований отвергнуть нулевую гипотезу, тренд отсутствует.")
-
-import numpy as np
-import matplotlib.pyplot as plt
-
-x = np.array([num for num in range(len(totals_only))])
-y = np.array(totals_only)
-
-
-plt.plot(x, y)
-plt.show()
-#print(f"P-value: {result.p}")
-
-import pandas as pd
-from scipy.stats import kendalltau
-
-# Example list of lists
-data = all_rows_as_list
-
-# Convert the list of lists to a DataFrame
-df = pd.DataFrame(data, columns=['Label', 'Timestamp', 'Value'])
-
-# Convert Timestamp column to datetime if needed
-df['Timestamp'] = pd.to_datetime(df['Timestamp'])
-
-# Compute Kendall's tau correlation between Timestamp and Value columns
-tau, p_value = kendalltau(df['Timestamp'], df['Value'])
-
-print(f"Kendall's Tau: {tau}")
-print(f"P-value: {p_value}")
+# integers_list = [sublist[2] for sublist in all_rows_as_list]
+#
+# from scipy.stats import kendalltau
+# import numpy as np
+# import matplotlib.pyplot as plt
+# from pandas import Timestamp
+#
+# # Ваш лист с данными
+# data = all_rows_as_list
+#
+# # Преобразование дат в формат Timestamp и количеств в массивы numpy
+# dates = np.array([row[1] for row in data])
+# qty = np.array([row[2] for row in data])
+#
+# # Выполнение теста Кендалла
+# tau, p_value = kendalltau(qty, range(len(qty)))
+#
+# # Определение направления тренда
+# trend_direction = "Upward" if tau > 0 else "Downward" if tau < 0 else "No Trend"
+#
+# # Вывод результатов
+# print(f"Trend Direction: {trend_direction}")
+# print(f"Kendall's Tau: {tau}")
+# print(f"P-value: {p_value}")
+#
+# # Постройте график данных
+# plt.plot(dates, qty, marker='o', label='Actual Demand')
+# plt.title('Demand Over Time')
+# plt.xlabel('Date')
+# plt.ylabel('Demand')
+# plt.legend()
+# plt.show()
+# import pandas as pd
+# from scipy.stats import kendalltau
+#
+# # Предположим, что у вас есть DataFrame с данными в листе1
+# # Замените 'your_data.csv' на путь к вашему файлу или используйте другие способы загрузки данных
+#
+# # Предположим, что у вас есть два столбца 'X' и 'Y', и вы хотите проверить тренд в 'Y' относительно 'X'
+# x_values = totals_only
+# y_values = [num for num in range(len(totals_only))]
+#
+# # Выполняем тест Кендалла
+# tau, p_value = kendalltau(x_values, y_values)
+#
+# # Выводим результаты теста
+# print(f"Значение статистики Кендалла (τ): {tau}")
+# print(f"P-значение: {p_value}")
+#
+# # Проверяем значимость
+# if p_value < 0.05:
+#     print("Отвергаем нулевую гипотезу, есть тренд.")
+# else:
+#     print("Нет оснований отвергнуть нулевую гипотезу, тренд отсутствует.")
+#
+# import numpy as np
+# import matplotlib.pyplot as plt
+#
+# x = np.array([num for num in range(len(totals_only))])
+# y = np.array(totals_only)
+#
+#
+# plt.plot(x, y)
+# plt.show()
+# #print(f"P-value: {result.p}")
+#
+# import pandas as pd
+# from scipy.stats import kendalltau
+#
+# # Example list of lists
+# data = all_rows_as_list
+#
+# # Convert the list of lists to a DataFrame
+# df = pd.DataFrame(data, columns=['Label', 'Timestamp', 'Value'])
+#
+# # Convert Timestamp column to datetime if needed
+# df['Timestamp'] = pd.to_datetime(df['Timestamp'])
+#
+# # Compute Kendall's tau correlation between Timestamp and Value columns
+# tau, p_value = kendalltau(df['Timestamp'], df['Value'])
+#
+# print(f"Kendall's Tau: {tau}")
+# print(f"P-value: {p_value}")
